db08cea93161b44e0c3496941fe227afa64805e0
Mohamed Elshowel <mohamed.elshowel@gmail.com>
2021-07-21 02:18:30 +0200

    feat: Move books between shelves

---
+++ src/components/book/Book.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as BooksAPI from '../../BooksAPI';
+
+function Book({ bookData, onUpdateShelves }) {
+
+    const onShelfChange = (evt) => {
+        const updatedShelfName = evt.target.value;
+
+        BooksAPI
+            .update(bookData, updatedShelfName).
+            then(updatedShelves => onUpdateShelves(updatedShelves));
+    }
+
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${bookData.imageLinks.thumbnail}")` }}></div>
+                <div className="book-shelf-changer">
+                    <select onChange={onShelfChange} defaultValue={bookData.shelf}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read" >Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
+            </div>
+            <div className="book-title">{bookData.title}</div>
+            <div className="book-authors">{bookData.authors.join(', ')}</div>
+        </div>
+    );
+}
+
+export default Book
---
+++ src/components/booksList/BooksList.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Book from '../book/Book';
+
+function BooksList({ books, shelves, onUpdateShelves }) {
+    return (
+        <div className="list-books-content">
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {shelves.currentlyReading.map(bookId => {
+                            const bookData = books.find(book => book.id === bookId);
+                            debugger;
+                            return (
+                                <li key={bookId}>
+                                    <Book bookData={bookData} onUpdateShelves={onUpdateShelves} />
+                                </li>
+                            )
+                        })}
+                    </ol>
+                </div>
+            </div>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {shelves.wantToRead.map(bookId => {
+                            const bookData = books.find(book => book.id === bookId);
+                            return (
+                                <li key={bookId}>
+                                    <Book bookData={bookData} onUpdateShelves={onUpdateShelves} />
+                                </li>
+                            )
+                        })}
+                    </ol>
+                </div>
+            </div>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {shelves.read.map(bookId => {
+                            const bookData = books.find(book => book.id === bookId);
+                            return (
+                                <li key={bookId}>
+                                    <Book bookData={bookData} onUpdateShelves={onUpdateShelves} />
+                                </li>
+                            )
+                        })}
+                    </ol>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BooksList
